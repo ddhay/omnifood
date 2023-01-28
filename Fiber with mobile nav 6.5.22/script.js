67755271046812db4e2e331aada47611a57aee6b
Darrell <ddhay41@gmail.com>
2023-01-28 12:28:51 -0600

    first commig

---
+++ Fiber with mobile nav 6.5.22/script.js
@@ -0,0 +1,6 @@
+// MAKE MOBILE NAVIGATION WORK WITH NAV-OPEN BUTTONS
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
