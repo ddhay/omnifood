b98e7656fa5ea25ad36a4529fc85e7f4ef6e548c
Darrell <ddhay41@gmail.com>
2023-01-29 07:57:45 -0600

    First commit

--- Fiber with mobile nav 6.5.22/script.js
+++
@@ -1,6 +0,0 @@
-// MAKE MOBILE NAVIGATION WORK WITH NAV-OPEN BUTTONS
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-Created in class taught by Jonas Schmedtmann on Udemy
